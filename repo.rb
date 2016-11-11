bbd8c89daaa5cdf3ace67326d31066f7795b62b3
byelipk <byelipk@gmail.com>
2016-11-10 19:24:11 -0500

    OK

--- repo.rb
+++ repo.rb
@@ -16,14 +16,14 @@ class Repo
         URI.parse(url)))
   end
 
-  def self.update_repo(url, options = Hash.new)
+  def self.update_repo(url, api_token, options = Hash.new)
     uri  = URI.parse(url)
     http = Net::HTTP.new(uri.host, uri.port)
     http.use_ssl = true
 
-    binding.pry
     request = Net::HTTP::Put.new(uri)
-    request.set_form_data(options)
+    request.add_field("Authorization", "token #{api_token}")
+    request.body = options.to_json
     response = http.request(request)
 
     puts response.body
